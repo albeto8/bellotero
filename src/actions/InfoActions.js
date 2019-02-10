6791df4c704d6bc388bbcc1f420e091322012697
Mario <>
2019-02-10 12:08:55 -0600

    Base url constant

--- src/actions/InfoActions.js
+++ src/actions/InfoActions.js
@@ -4,13 +4,14 @@ import {
   GLOBAL_INFO_LOAD_SUCCESS,
   GLOBAL_INFO_LOAD_FAIL
 } from './types';
+import { BASE_URL } from '../api'
 
 export const getGlobalInfo = () => async (dispatch) => {
   dispatch({ type: GLOBAL_INFO_LOADING });
   try {
-    const response = await axios.get('https://raw.githubusercontent.com/Bernabe-Felix/Bellotero/master/app.json')
+    const response = await axios.get(`${BASE_URL}/app.json`)
     dispatch({ type: GLOBAL_INFO_LOAD_SUCCESS, payload: response.data });
   } catch (e) {
     dispatch({ type: GLOBAL_INFO_LOAD_FAIL, payload: `${e}` });
   }
-}
+};
