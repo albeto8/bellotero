6ca538e5840f473c0929d949a9fe93b96a2cf790
Mario <>
2019-02-10 19:14:06 -0600

    Material UI Slider, Configurator state values

--- src/components/Configurator.js
+++ src/components/Configurator.js
@@ -1,17 +1,85 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import Slider from '@material-ui/lab/Slider';
 import { getConfiguratorInfo } from '../actions';
 
 class Configurator extends React.Component {
+
+  state = {
+    fullTimeEmployees: 1,
+    monthlyIngredientSpending: 10,
+    foodCostSaving: 10 * 0.3
+  }
 
   componentDidMount() {
     this.props.getConfiguratorInfo();
   }
 
+  handleEmployeesChange = (event, value) => {
+    this.setState({ fullTimeEmployees: value });
+  }
+
+  handleMonthChange = (event, value) => {
+    this.setState({ monthlyIngredientSpending: value, foodCostSaving: value * 0.3 });
+  }
+
+  renderFoodCostSaving() {
+    const { foodCostSaving } = this.state;
+    return (
+      <p>Estimated Food Cost Saving {foodCostSaving}</p>
+    );
+  }
+
+  renderAnnualSavings() {
+    const { foodCostSaving, fullTimeEmployees } = this.state;
+    return (
+      <p>Your estimated annual savings {fullTimeEmployees * 1337 + foodCostSaving}</p>
+    );
+  }
+
+  renderEmployeesSlider() {
+    const { fullTimeEmployees } = this.state;
+    return (
+      <div>
+        <p>Employees Slider {fullTimeEmployees}</p>
+        <Slider
+          min={1}
+          max={10}
+          step={1}
+          value={fullTimeEmployees}
+          aria-labelledby="label"
+          onChange={this.handleEmployeesChange}
+        />
+      </div>
+    );
+  }
+
+  renderMonthSlider() {
+    const { monthlyIngredientSpending } = this.state;
+    return (
+      <div>
+        <p>Monthly Ingredient spending {monthlyIngredientSpending}</p>
+        <Slider
+          min={10}
+          max={100}
+          value={monthlyIngredientSpending}
+          aria-labelledby="label"
+          onChange={this.handleMonthChange}
+        />
+      </div>
+    );
+  }
+
   render() {
     const { title } = this.props.configuratorInfo.calculator;
     return (
-      <div>{title}</div>
+      <div>
+        {title}
+        {this.renderEmployeesSlider()}
+        {this.renderMonthSlider()}
+        {this.renderFoodCostSaving()}
+        {this.renderAnnualSavings()}
+      </div>
     );
   }
 }
