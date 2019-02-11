7512e5c33e5a44f5baec92a92213de4b3168e511
Mario <>
2019-02-11 16:36:07 -0600

    Change Slider label font

--- src/components/SliderLabel.js
+++ src/components/SliderLabel.js
@@ -24,7 +24,7 @@ const sliderContainer = {
 
 const labelTextStyle = {
   fontFamily: 'Roboto',
-  fontWeight: 500,
+  fontWeight: 700,
   fontSize: '14px',
   display: 'flex',
   alignItems: 'center',
